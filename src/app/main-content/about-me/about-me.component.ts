a6dbbf594aeb1aeb59c4b4f50b05eb8f324dbcd8
Stefan Helldobler <duebel3@googlemail.com>
2025-07-30 18:02:34 +0200

    feat : add landing-page;about-me; tech-steck components

--- src/app/main-content/about-me/about-me.component.ts
+++ src/app/main-content/about-me/about-me.component.ts
@@ -10,4 +10,26 @@ import { CommonModule } from '@angular/common';
 })
 export class AboutMeComponent {
   hoveredIcon: string | null = null;
+
+  icons = [
+  {
+    name: 'map-icon-green',
+    text: ' Where are you located? Are you open to different ways of working, such as working remotely or even relocating?'
+  },
+  {
+    name: 'light-bulb-green',
+    text: '   Show that you are open-minded. Are you enthusiastic about learning new technologies and continually improving your skills?'
+  },
+  {
+    name: 'puzzle-green',
+    text: 'A brief description of your problem-solving approach. Do you learnfrom each challenge as you search for the most efficient or elegantsolution? You can include some keywords like: analytical thinking, creativity, persistence and collaboration.'
+  }
+];
+
+getIconSrc(name: string): string {
+  const base = 'assets/icons/about-me-icons/';
+  return this.hoveredIcon === name.replace('-green', '') // "map", "puzzle", etc.
+    ? `${base}${name}-hovered.png`
+    : `${base}${name}.png`;
+}
 }
--- src/app/main-content/tech-stack/tech-stack.component.ts
+++ src/app/main-content/tech-stack/tech-stack.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tech-stack',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tech-stack.component.html',
   styleUrl: './tech-stack.component.scss'
 })
 export class TechStackComponent {
+  hoveredSkill: string | null = null;
 
+  skills = [
+    { name: 'css', displayName: 'CSS' },
+    { name: 'javascript', displayName: 'JavaScript' },
+    { name: 'typescript', displayName: 'TypeScript' },
+    { name: 'firebase', displayName: 'Firebase' },
+    { name: 'git', displayName: 'Git' },
+    { name: 'rest-api', displayName: 'REST-API' },
+    { name: 'material-design', displayName: 'Material Design' },
+    { name: 'continually-learning', displayName: 'Continually learning' }
+  ];
+
+  getSkillIconSrc(name: string): string {
+    const base = 'assets/icons/skill-icons/';
+    return this.hoveredSkill === name
+      ? `${base}${name}-hovered.png`
+      : `${base}${name}.png`;
+  }
 }
