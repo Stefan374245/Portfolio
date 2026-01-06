72a341d20c32de06e6f207d29da1c7ce38c4a907
Stefan Helldobler <shelldobler@gmx.de>
2026-01-06 20:05:02 +0100

    update: modify project service configuration

--- src/app/shared/services/project.service.ts
+++ src/app/shared/services/project.service.ts
@@ -9,17 +9,27 @@ export class ProjectService {
   private projects: Project[] = [
     {
       id: 'join',
-      name: 'Join',
-      tech: 'Angular | TypeScript | HTML | CSS | Firebase',
+      name: 'Join-Issue-Collector',
+      tech: ' HTML | Angular | TypeScript | N8N  | Firebase | CDK',
       descriptionKey: 'projects.join.description',
       image: 'assets/images/portfolio-section/screen-join.png',
        video: 'assets/videos/join-preview.mp4',
-      liveUrl: 'https://join.stefan-helldobler.de/',
-      githubUrl: 'https://github.com/Stefan374245/Join-Taskmanagement.git'
+      liveUrl: 'https://stefan-helldobler.de/join-issuecollector',
+      githubUrl: 'https://github.com/Stefan374245/Join'
+    },
+     {
+      id: 'travel2speak',
+      name: 'Travel2Speak',
+      tech: 'HTML | React | Tailwind CSS 3 | Gemini AI API',
+      descriptionKey: 'projects.travel2speak.description',
+      image: 'assets/images/portfolio-section/screen-travel2speak.png',
+      video: 'assets/videos/travel2speak-preview.mp4',
+      liveUrl: 'https://stefan-helldobler.de/travel2speak/',
+      githubUrl: 'https://github.com/Stefan374245/AI-Travel-Diary'
     },
     {
       id: 'el-pollo-loco',
-      name: 'El Pollo Loco', 
+      name: 'El Pollo Loco',
       tech: 'JavaScript | HTML | CSS',
       descriptionKey: 'projects.elPolloLoco.description',
       image: 'assets/images/portfolio-section/screen-el-pollo.png',
@@ -30,23 +40,14 @@ export class ProjectService {
     {
       id: 'pokedex',
       name: 'Pokedex',
-      tech: 'HTML | JavaScript | CSS | API', 
+      tech: 'HTML | JavaScript | CSS | API',
       descriptionKey: 'projects.pokedex.description',
       image: 'assets/images/portfolio-section/screen-pokedex.png',
        video: 'assets/videos/pokedex-preview.mp4',
       liveUrl: 'https://pokedex.stefan-helldobler.de',
       githubUrl: 'https://github.com/Stefan374245/pok-dex-project.git'
     },
-    {
-      id: 'simple-crm',
-      name: 'Simple CRM',
-      tech: 'Angular | TypeScript | HTML | CSS | Firebase', 
-      descriptionKey: 'projects.simpleCrm.description',
-      image: 'assets/images/portfolio-section/screen-simple-crm.png',
-      video: 'assets/videos/simple-crm-preview.mp4',
-      liveUrl: 'https://simple-crm.stefan-helldobler.de',
-      githubUrl: 'https://github.com/Stefan374245/simple-crm.git'
-    }
+
   ];
 
   getProjects(): Observable<Project[]> {
